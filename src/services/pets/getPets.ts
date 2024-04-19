db839673fb424c7c5a33240a2f1e61208783de51
Pedro-Moretti <moretti.drope@gmail.com>
2024-04-18 22:23:27 -0300

    Feat: Adicionado paginação e finalização de conexão com o DB

--- src/services/pets/getPets.ts
+++ src/services/pets/getPets.ts
@@ -1,14 +1,16 @@
 import { GetPetsRequest, GetPetsResponse } from '../../interfaces/pet'
 import httpClient from '../api/httpClient'
 
-export async function getPets(params?: GetPetsRequest): Promise<GetPetsResponse> {
-    try {
-        const response = await httpClient.get('/pet', { params })
-        return response.data
+export async function getPets(
+  params?: GetPetsRequest
+): Promise<GetPetsResponse> {
+  try {
+    // await new Promise((resolve) => setTimeout(resolve, 2000))
+    const response = await httpClient.get('/pet', { params })
 
-    } catch (error) {
-        console.log('ERRO AO BUSCAR PETS', error)
-        throw error
-    }
-
+    return response.data
+  } catch (error) {
+    console.log('Erro ao buscar pets', error)
+    throw error
+  }
 }
