83bbbd3f6317e0c99249994c1b63558b74c97d14
Pedro-Moretti <moretti.drope@gmail.com>
2024-04-23 22:16:14 -0300

    Feat: Componentes do Input

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -1,5 +1,19 @@
 import { Panel } from "../../../components/layout/Panel";
+import { Input } from "../../../components/common/Input/Input";
+import styles from './Shelter.module.css'
+import { Button } from "../../../components/common/Button";
 
 export function Shelter() {
-    return <Panel>Formulário de abrigo</Panel>
+    function submit(event) {
+        event.
+    }
+    return (
+        <form className={styles.container} onSubmit={submit}>
+            <Panel>
+            <Input label="Nome" value="PICLES VERDE"/>
+            <Button type="submit">Salvar dados</Button>
+        </Panel>
+        </form>
+        
+    )
 }
